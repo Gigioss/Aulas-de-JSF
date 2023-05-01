37e79def3b7d57eb35f06225c16da86705f73002
Gigioss <gigiosinicasi@gmail.com>
2023-05-01 15:00:20 -0300

    Teste agenda
    agenda

---
+++ Pacientes/src/java/br/test/objetos/entidade/Paciente.java
@@ -0,0 +1,74 @@
+
+package br.test.objetos.entidade;
+import java.util.Objects;
+
+public class Paciente {
+  private Integer id;
+  private String nome;
+  private String telefone;
+  private String email;
+  
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Paciente other = (Paciente) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+   
+  
+}
---
+++ Pacientes/src/java/br/test/objetos/util/exception/ErroSistema.java
@@ -0,0 +1,15 @@
+package br.test.objetos.util.exception;
+
+public class ErroSistema extends Exception{
+
+    public ErroSistema(String message) {
+        super(message);
+    }
+
+    public ErroSistema(String message, Throwable cause) {
+         super(message,cause);
+    }
+    
+    
+    
+}
